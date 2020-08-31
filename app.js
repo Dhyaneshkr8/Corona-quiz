90de4744de2253f291fd01103fec76e125d4d44c
Dhyaneshkr8 <63092167+Dhyaneshkr8@users.noreply.github.com>
2020-08-31 18:23:20 +0530

    Commit Spider basic to hacker mode

--- app.js
+++ app.js
@@ -2,6 +2,53 @@ var score = 0;
 
 var clicked = 0;
 
+var myName;
+
+var endTimer = 0;
+
+
+var presentDate = new Date().toLocaleDateString();
+var presentTime = new Date().toLocaleTimeString();
+
+var storeScore1;
+var storeScore2;
+var storeScore3;
+var storeScore4;
+var storeScore5;
+
+var getScore1;
+var getScore2;
+var getScore3;
+var getScore4;
+var getScore5;
+
+
+var storeName1;
+var storeName2;
+var storeName3;
+var storeName4;
+var storeName5;
+
+var getName1;
+var getName2;
+var getName3;
+var getName4;
+var getName5;
+
+var storedateTime1;
+var storedateTime2;
+var storedateTime3;
+var storedateTime4;
+var storedateTime5;
+
+var getdateTime1;
+var getdateTime2;
+var getdateTime3;
+var getdateTime4;
+var getdateTime5;
+
+
+
 var answer = [{
     qn: "Can washing your hands protect you from COVID-19?",
     op1: "1)   Yes – but only if you use a strong bleach",
@@ -86,6 +133,7 @@ var answer = [{
 
 
 
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -119,6 +167,31 @@ function showSlides(n) {
 
 
 
+
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+shuffle(answer);
+
+
+
+
+
+
 for (var j = 1; j < 11; j++) {
   document.querySelector(".qn" + String(j)).innerHTML = answer[j - 1].qn;
   document.querySelector(".op1" + String(j)).innerHTML = answer[j - 1].op1;
@@ -129,10 +202,15 @@ for (var j = 1; j < 11; j++) {
 
 
 
+
+
 document.querySelector(".start").addEventListener("click", function() {
-  // alert("in");
 
   document.querySelector(".start").classList.add("hide");
+  document.querySelector(".start").classList.remove("margin");
+  document.querySelector(".enter").classList.add("hide");
+  document.querySelector(".name").classList.add("hide");
+  document.querySelector(".timer").classList.remove("hide");
   document.querySelector(".stat").textContent = "3";
   document.querySelector(".stat").style.fontSize = "100px";
   document.querySelector(".stat").style.color = "white";
@@ -151,12 +229,21 @@ document.querySelector(".start").addEventListener("click", function() {
   setTimeout(function() {
 
     document.querySelector(".stat").classList.add("hide");
+
+    myName = String(document.querySelector(".name").value);
     document.querySelector(".slideshow-container").classList.remove("hide");
     document.querySelector(".mover").classList.remove("hide");
 
     document.querySelector(".submit").classList.remove("hide");
+    document.querySelector(".sidebar").classList.remove("hide");
+    var time_in_minutes = 3;
+    var current_time = Date.parse(new Date());
+    var deadline = new Date(current_time + time_in_minutes * 60 * 1000);
+    run_clock('clockdiv', deadline);
+
   }, 3000);
 });
+
 
 
 
@@ -425,17 +512,203 @@ for (var i = 1; i < 5; i++) {
 document.querySelector(".submit").addEventListener("click", myFunction2);
 
 
-function myFunction2()
-{
+
+function myFunction2() {
+  endTimer = 1;
   document.querySelector(".quiz").classList.add("hide");
+  document.querySelector(".timer").classList.add("hide");
   document.querySelector(".submit").classList.add("hide");
   document.querySelector(".slideshow-container").classList.add("hide");
   document.querySelector(".mover").classList.add("hide");
+  document.querySelector(".sidebar").classList.add("hide");
   document.querySelector(".scoreBoard").classList.remove("hide");
 
-  document.querySelector(".scoring").innerHTML = "SCORE  :   " + String(score) + "  out  of  10";
-
+  if (myName == "") {
+    document.querySelector(".nameing").innerHTML = "NAME  :    No name";
+    document.querySelector(".scoring").innerHTML = "SCORE  :   " + String(score) + "  out  of  10";
+    myName = "No name";
+  } else {
+    document.querySelector(".nameing").innerHTML = "NAME  :       " + myName.toUpperCase();
+    document.querySelector(".scoring").innerHTML = "SCORE  :      " + String(score) + "  out  of  10";
+  }
   document.querySelector(".crtQns").innerHTML = "No.  of   CORRECT   questions  :  " + String(score);
   document.querySelector(".ratiocrt").innerHTML = "CORRECT   to   WRONG   ratio  :  " + String(score / (10 - score));
 
+  compare();
 }
+
+
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+  if (document.querySelector(".dropbtn").innerHTML == "⬇ Questions") {
+    document.querySelector(".dropbtn").innerHTML = "⬆ Questions";
+  } else {
+    document.querySelector(".dropbtn").innerHTML = "⬇ Questions"
+  }
+}
+
+
+
+
+
+function time_remaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function run_clock(id, endtime) {
+  var clock = document.getElementById(id);
+
+  function update_clock() {
+    var t = time_remaining(endtime);
+    clock.innerHTML = "0" + t.minutes + ' : ' + t.seconds;
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+    if (t.minutes == 00 && t.seconds == 00 && endTimer == 0) {
+      alert("Time is up");
+      myFunction2();
+    }
+  }
+
+  update_clock(); // run function once at first to avoid delay
+  var timeinterval = setInterval(update_clock, 1000);
+}
+
+
+
+
+
+
+
+
+function compare() {
+  // alert(document.querySelector(".score1").innerHTML);
+  if (score >= parseInt(document.querySelector(".score1").innerHTML)) {
+    document.querySelector(".score5").innerHTML = document.querySelector(".score4").innerHTML;
+    document.querySelector(".score4").innerHTML = document.querySelector(".score3").innerHTML;
+    document.querySelector(".score3").innerHTML = document.querySelector(".score2").innerHTML;
+    document.querySelector(".score2").innerHTML = document.querySelector(".score1").innerHTML;
+    document.querySelector(".score1").innerHTML = String(score);
+
+    document.querySelector(".name5").innerHTML = document.querySelector(".name4").innerHTML;
+    document.querySelector(".name4").innerHTML = document.querySelector(".name3").innerHTML;
+    document.querySelector(".name3").innerHTML = document.querySelector(".name2").innerHTML;
+    document.querySelector(".name2").innerHTML = document.querySelector(".name1").innerHTML;
+    document.querySelector(".name1").innerHTML = myName;
+
+    document.querySelector(".dateTime5").innerHTML = document.querySelector(".dateTime4").innerHTML;
+    document.querySelector(".dateTime4").innerHTML = document.querySelector(".dateTime3").innerHTML;
+    document.querySelector(".dateTime3").innerHTML = document.querySelector(".dateTime2").innerHTML;
+    document.querySelector(".dateTime2").innerHTML = document.querySelector(".dateTime1").innerHTML;
+    document.querySelector(".dateTime1").innerHTML = presentDate + " " + presentTime;
+
+  } else if ((score < parseInt(document.querySelector(".score1").innerHTML)) && (score >= parseInt(document.querySelector(".score2").innerHTML))) {
+    document.querySelector(".score5").innerHTML = document.querySelector(".score4").innerHTML;
+    document.querySelector(".score4").innerHTML = document.querySelector(".score3").innerHTML;
+    document.querySelector(".score3").innerHTML = document.querySelector(".score2").innerHTML;
+    document.querySelector(".score2").innerHTML = String(score);
+
+    document.querySelector(".name5").innerHTML = document.querySelector(".name4").innerHTML;
+    document.querySelector(".name4").innerHTML = document.querySelector(".name3").innerHTML;
+    document.querySelector(".name3").innerHTML = document.querySelector(".name2").innerHTML;
+    document.querySelector(".name2").innerHTML = myName;
+
+    document.querySelector(".dateTime5").innerHTML = document.querySelector(".dateTime4").innerHTML;
+    document.querySelector(".dateTime4").innerHTML = document.querySelector(".dateTime3").innerHTML;
+    document.querySelector(".dateTime3").innerHTML = document.querySelector(".dateTime2").innerHTML;
+    document.querySelector(".dateTime2").innerHTML = presentDate + " " + presentTime;
+  } else if ((score < parseInt(document.querySelector(".score2").innerHTML)) && (score >= parseInt(document.querySelector(".score3").innerHTML))) {
+    document.querySelector(".score5").innerHTML = document.querySelector(".score4").innerHTML;
+    document.querySelector(".score4").innerHTML = document.querySelector(".score3").innerHTML;
+    document.querySelector(".score3").innerHTML = String(score);
+
+    document.querySelector(".name5").innerHTML = document.querySelector(".name4").innerHTML;
+    document.querySelector(".name4").innerHTML = document.querySelector(".name3").innerHTML;
+    document.querySelector(".name3").innerHTML = myName;
+
+    document.querySelector(".dateTime5").innerHTML = document.querySelector(".dateTime4").innerHTML;
+    document.querySelector(".dateTime4").innerHTML = document.querySelector(".dateTime3").innerHTML;
+    document.querySelector(".dateTime3").innerHTML = presentDate + " " + presentTime;
+  } else if ((score < parseInt(document.querySelector(".score3").innerHTML)) && (score >= parseInt(document.querySelector(".score4").innerHTML))) {
+    document.querySelector(".score5").innerHTML = document.querySelector(".score4").innerHTML;
+    document.querySelector(".score4").innerHTML = String(score);
+
+    document.querySelector(".name5").innerHTML = document.querySelector(".name4").innerHTML;
+    document.querySelector(".name4").innerHTML = myName;
+
+    document.querySelector(".dateTime5").innerHTML = document.querySelector(".dateTime4").innerHTML;
+    document.querySelector(".dateTime4").innerHTML = presentDate + " " + presentTime;
+  } else if ((score < parseInt(document.querySelector(".score4").innerHTML)) && (score >= parseInt(document.querySelector(".score5").innerHTML))) {
+    document.querySelector(".score5").innerHTML = String(score);
+
+    document.querySelector(".name5").innerHTML = myName;
+
+    document.querySelector(".dateTime5").innerHTML = presentDate + " " + presentTime;
+  }
+  localStorage.setItem('storeScore1', document.querySelector(".score1").innerHTML);
+  localStorage.setItem('storeScore2', document.querySelector(".score2").innerHTML);
+  localStorage.setItem('storeScore3', document.querySelector(".score3").innerHTML);
+  localStorage.setItem('storeScore4', document.querySelector(".score4").innerHTML);
+  localStorage.setItem('storeScore5', document.querySelector(".score5").innerHTML);
+
+  localStorage.setItem('storeName1', document.querySelector(".name1").innerHTML);
+  localStorage.setItem('storeName2', document.querySelector(".name2").innerHTML);
+  localStorage.setItem('storeName3', document.querySelector(".name3").innerHTML);
+  localStorage.setItem('storeName4', document.querySelector(".name4").innerHTML);
+  localStorage.setItem('storeName5', document.querySelector(".name5").innerHTML);
+
+  localStorage.setItem('storedateTime1', document.querySelector(".dateTime1").innerHTML);
+  localStorage.setItem('storedateTime2', document.querySelector(".dateTime2").innerHTML);
+  localStorage.setItem('storedateTime3', document.querySelector(".dateTime3").innerHTML);
+  localStorage.setItem('storedateTime4', document.querySelector(".dateTime4").innerHTML);
+  localStorage.setItem('storedateTime5', document.querySelector(".dateTime5").innerHTML);
+}
+
+getScore1 = localStorage.getItem('storeScore1');
+getScore2 = localStorage.getItem('storeScore2');
+getScore3 = localStorage.getItem('storeScore3');
+getScore4 = localStorage.getItem('storeScore4');
+getScore5 = localStorage.getItem('storeScore5');
+
+getName1 = localStorage.getItem('storeName1');
+getName2 = localStorage.getItem('storeName2');
+getName3 = localStorage.getItem('storeName3');
+getName4 = localStorage.getItem('storeName4');
+getName5 = localStorage.getItem('storeName5');
+
+getdateTime1 = localStorage.getItem('storedateTime1');
+getdateTime2 = localStorage.getItem('storedateTime2');
+getdateTime3 = localStorage.getItem('storedateTime3');
+getdateTime4 = localStorage.getItem('storedateTime4');
+getdateTime5 = localStorage.getItem('storedateTime5');
+
+
+document.querySelector(".score1").innerHTML = getScore1;
+document.querySelector(".score2").innerHTML = getScore2;
+document.querySelector(".score3").innerHTML = getScore3;
+document.querySelector(".score4").innerHTML = getScore4;
+document.querySelector(".score5").innerHTML = getScore5;
+
+document.querySelector(".name1").innerHTML = getName1;
+document.querySelector(".name2").innerHTML = getName2;
+document.querySelector(".name3").innerHTML = getName3;
+document.querySelector(".name4").innerHTML = getName4;
+document.querySelector(".name5").innerHTML = getName5;
+
+document.querySelector(".dateTime1").innerHTML = getdateTime1;
+document.querySelector(".dateTime2").innerHTML = getdateTime2;
+document.querySelector(".dateTime3").innerHTML = getdateTime3;
+document.querySelector(".dateTime4").innerHTML = getdateTime4;
+document.querySelector(".dateTime5").innerHTML = getdateTime5;
